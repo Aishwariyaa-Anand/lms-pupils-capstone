2fd53cb6cbd0969a885ce12b2776c19787186dd0
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-01-29 21:51:50 +0530

    added relationships to the tables

---
+++ migrations/20240129155925-allrelations-lms-db.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    //educators id in course table
+    await queryInterface.addColumn('courses', 'eduId', {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'educators',
+        key: 'id',
+      },
+    });
+
+    //courseId in Chapters table
+    await queryInterface.addColumn('chapters', 'courseId', {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'courses',
+        key: 'id',
+      },
+    });
+
+    //chapterId in pages table
+    await queryInterface.addColumn('pages', 'chapterId', {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'chapters',
+        key: 'id',
+      },
+    });
+
+    //many-to-many relationship btw students and courses
+    await queryInterface.createTable('student_course', {
+      studentId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'students',
+          key: 'id',
+        },
+      },
+      courseId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'courses',
+          key: 'id',
+        },
+      },
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('pages', 'chapterId');
+
+    await queryInterface.removeColumn('chapters', 'courseId');
+
+    await queryInterface.removeColumn('courses', 'eduId');
+
+    await queryInterface.dropTable('student_course');
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
