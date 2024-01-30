638edde04117261ed637f2fb27a9336f78b55736
Aishwariyaa_Anand <aishwariyaa.anand@gmail.com>
2024-01-30 21:00:43 +0530

    adding colunm completed for pages table

--- migrations/20240129154237-create-page.js
+++ migrations/20240129154237-create-page.js
@@ -16,6 +16,10 @@ module.exports = {
       content: {
         type: Sequelize.STRING
       },
+      completed:{
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
